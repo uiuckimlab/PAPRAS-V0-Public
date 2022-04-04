3aa67e2c6e55d9ec18901305c5ef7ff175fdf7ae
Victoria Colthurst <vrc2@illinois.edu>
2022-04-04 10:47:27 -0500

    moved script to demo folder

--- chess_demo/launch_chess_script.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting chess demo!"
-
-echo "Launching RVIZ"
-roslaunch demo_chess _demo_chess.launch
-
-# PAPRAS/moveit_configs/scan_object_moveit_config/config/open_manipulator_p.srdf
-# PAPRAS/papras_moveit_config/tea_table_moveit_config/config/tea_table.srdf 
