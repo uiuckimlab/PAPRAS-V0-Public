a89611607624c805ad9f49c77791e15e7acd8a34
Kazuki Shin <kazukis2@illinois.edu>
2022-05-01 22:57:19 -0500

    Added initial work for launching all the files

--- papras_utils/papras_utils/scripts/homecare_launch.py
+++ papras_utils/papras_utils/scripts/homecare_launch.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python3
+
 import roslaunch
 import rospy
 
-rospy.init_node('en_Mapping', anonymous=True)
+rospy.init_node('homecare_node', anonymous=True)
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 
 home_path = "/home/kimlab"
 
-
-cli_args = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/papras_homecare_handoff_fsm.launch']
+cli_args = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/_demo_homecare_hw_big_table.launch']
 roslaunch_args = cli_args[1:]
+# cli_args_handoff = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/papras_homecare_handoff_fsm.launch']
+# roslaunch_args_handoff = cli_args_handoff[1:]
 roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
 
 launch = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
+
+
 launch.start()
 rospy.loginfo("started")
+rospy.sleep(rospy.Duration(15))
+# launch_pick_place.start()
 
 try:
   launch.spin()
