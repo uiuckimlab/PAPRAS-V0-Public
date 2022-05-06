e6ac981002a5fd5b61db77153c00538e56c9d19e
Kazuki Shin <kazukis2@illinois.edu>
2022-05-05 22:12:51 -0500

    [roomba nav] roomba demo successful

--- papras_utils/papras_utils/scripts/homecare_launch.py
+++ papras_utils/papras_utils/scripts/homecare_launch.py
@@ -7,9 +7,9 @@ rospy.init_node('homecare_node', anonymous=True)
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 
-home_path = "/home/kimlab"
+home_path = "/home/papras"
 
-cli_args = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/_demo_homecare_hw_big_table.launch']
+cli_args = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/roomba_nav.launch']
 roslaunch_args = cli_args[1:]
 # cli_args_handoff = [home_path+'/catkin_ws/src/PAPRAS/papras_demo/launch/papras_homecare_handoff_fsm.launch']
 # roslaunch_args_handoff = cli_args_handoff[1:]
@@ -20,8 +20,6 @@ launch = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
 
 launch.start()
 rospy.loginfo("started")
-rospy.sleep(rospy.Duration(15))
-# launch_pick_place.start()
 
 try:
   launch.spin()
