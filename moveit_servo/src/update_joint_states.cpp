0a8ce78f22b46586bf6570d06cbf64dc36f4235b
Kazuki Shin <kazukis2@illinois.edu>
2022-11-16 12:56:07 -0600

    Changes so it doesnt start at init. Can start anywhere

--- moveit_servo/src/update_joint_states.cpp
+++ moveit_servo/src/update_joint_states.cpp
@@ -1,4 +1,4 @@
-/* Author: Sankalp Yamsani*/
+/* Author: Sankalp Yamsani and Kazuki Shin*/
 #include <ros/ros.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <control_msgs/JointJog.h>
@@ -13,11 +13,15 @@
 // Some constants used in the Servo Teleop demo
 const std::string JOINT_STATE_TOPIC = "/joint_states";
 const std::string ARM1_CONTROLLER_TOPIC = "/arm1_controller/command";
+const std::string ARM2_CONTROLLER_TOPIC = "/arm2_controller/command";
 const std::string MANUAL_JOINT_STATE_TOPIC = "/servo_joint_states";
+
 
 ros::Publisher new_joint_state_pub_;
 sensor_msgs::JointState current_joint_state; 
-std::unordered_map<std::string, int> indexes;
+std::unordered_map<std::string, int> indexes_arm1;
+std::unordered_map<std::string, int> indexes_arm2;
+std::unordered_map<std::string, double> last_joint_values;
 
 
 void joint_state_callback(const sensor_msgs::JointState msg) {
@@ -27,20 +31,40 @@ void joint_state_callback(const sensor_msgs::JointState msg) {
 
 void arm1_command_callback(const trajectory_msgs::JointTrajectory msg) {
     sensor_msgs::JointState publish_joint_state = current_joint_state;
-    
     for(size_t i = 0; i < msg.joint_names.size(); i++){
-        indexes[msg.joint_names[i]] = i;
-    }
-    
-    for(size_t i = 0; i < publish_joint_state.name.size(); i++){
-        if(indexes.find(publish_joint_state.name[i]) != indexes.end()){
-            publish_joint_state.position[i] = msg.points[msg.points.size()-1].positions[indexes[publish_joint_state.name[i]]];
-        }
-        publish_joint_state.velocity[i] = 0;
+        indexes_arm1[msg.joint_names[i]] = i;
     }
 
+    for(size_t i = 0; i < publish_joint_state.name.size(); i++){
+        if(indexes_arm1.find(publish_joint_state.name[i]) != indexes_arm1.end()){
+            publish_joint_state.position[i] = msg.points[msg.points.size()-1].positions[indexes_arm1[publish_joint_state.name[i]]];
+            last_joint_values[publish_joint_state.name[i]] = publish_joint_state.position[i];
+        }else if(last_joint_values.find(publish_joint_state.name[i]) != last_joint_values.end()){
+            publish_joint_state.position[i] = last_joint_values[publish_joint_state.name[i]];
+            last_joint_values[publish_joint_state.name[i]] = publish_joint_state.position[i];
+        }
+    }
     new_joint_state_pub_.publish(publish_joint_state);
 
+}
+
+
+void arm2_command_callback(const trajectory_msgs::JointTrajectory msg){
+    sensor_msgs::JointState publish_joint_state = current_joint_state;
+    for(size_t i = 0; i < msg.joint_names.size(); i++){
+        indexes_arm2[msg.joint_names[i]] = i;
+    }
+
+    for(size_t i = 0; i < publish_joint_state.name.size(); i++){
+        if(indexes_arm2.find(publish_joint_state.name[i]) != indexes_arm2.end()){
+            publish_joint_state.position[i] = msg.points[msg.points.size()-1].positions[indexes_arm2[publish_joint_state.name[i]]];
+            last_joint_values[publish_joint_state.name[i]] = publish_joint_state.position[i];
+        }else if(last_joint_values.find(publish_joint_state.name[i]) != last_joint_values.end()){
+            publish_joint_state.position[i] = last_joint_values[publish_joint_state.name[i]];
+            last_joint_values[publish_joint_state.name[i]] = publish_joint_state.position[i];
+        }
+    }
+    new_joint_state_pub_.publish(publish_joint_state);
 }
 
 
@@ -57,17 +81,17 @@ int main(int argc, char** argv)
 
     ros::Subscriber joint_state_sub = nh.subscribe(JOINT_STATE_TOPIC,10, joint_state_callback);
     ros::Subscriber arm1_command = nh.subscribe(ARM1_CONTROLLER_TOPIC,10,arm1_command_callback);
+    ros::Subscriber arm2_command = nh.subscribe(ARM2_CONTROLLER_TOPIC,10,arm2_command_callback);
 
     
     sensor_msgs::JointState publish_joint_state = current_joint_state;
 
-    for(size_t i = 0; i < publish_joint_state.name.size(); i++){
-        if(indexes.find(publish_joint_state.name[i]) != indexes.end()){
-            publish_joint_state.position[i] = 0;
-        }
-        publish_joint_state.velocity[i] = 0;
+    while(indexes_arm1.size() == 0 && indexes_arm2.size() == 0){
+        publish_joint_state = current_joint_state;
+        new_joint_state_pub_.publish(publish_joint_state);
+        ros::Duration(0.1).sleep();
+
     }
-    new_joint_state_pub_.publish(publish_joint_state);
     
     ros::waitForShutdown();
 }
